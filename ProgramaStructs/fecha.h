45c37e2fcc4a2542eb21d172565a32c1985fc690
Teo Laírla <teo.lairla@iessierradeguara.com>
2017-10-24 11:12:24 +0200

    Modificación librería fecha

--- ProgramaStructs/fecha.h
+++ ProgramaStructs/fecha.h
@@ -1,8 +1,43 @@
 #ifndef FECHA_H
 #define FECHA_H
 
-/**struct s_fecha
-{
+/*
+La librería stdbool.h da la 
+funcionalidad de las variables
+booleanas. true y false.
+el tipo de datos será bool
+*/
+
+#include <stdbool.h>
+
+/*
+//A partir de aquí podemos
+//usar cosas como: 
+bool correcto;
+correcto = true;
+correcto = false;
+if(!correcto){
+	
+}
+
+while(correcto){
+	correcto = !correcto;
+}
+
+*/
+
+//Definimos el contenido
+//de la estructura fecha.
+typedef struct{
+	short int anyo;
+	short int mes;
+	short int dia;
+}fecha;
+
+/*
+//Esto es equivalente 
+//a lo de arriba:
+struct s_fecha{
 	short int anyo;
 	short int mes;
 	short int dia;
@@ -12,22 +47,31 @@ typedef struct s_fecha fecha;
 
 */
 
-typedef struct{
-	short int anyo;
-	short int mes;
-	short int dia;
-}fecha;
+//Devuelve true si la fecha es correcta
+//Devuelve false si la fecha no lo es.
+bool fecha_correcta(fecha);
 
+//Devuelve true si el año pasado por parámetro
+// es bisiesto y false en caso contrario.
+bool anyo_bisiesto(short int anyo);
 
-//Devuelve 0 si la fecha es correcta.
-int fecha_correcta(fecha);
-int anyo_bisiesto(short int);
+//Devuelve la fecha correspondiente
+//al día siguiente del parámetro
+//La fecha será válida.
 fecha dia_siguiente(fecha);
+
+//Devuelve la fecha correspondiente
+// al día anterior del parametro
+//La fecha será válida.
 fecha dia_anterior(fecha);
-fecha crear_fecha(short int, short int, short int);
 
-
-
-
+//Devuelve una estructura
+//de tipo fecha con los valores
+//anyo, mes y dia pasados por parámetro.
+//En el caso de que la fecha no sea correcta
+//se imprime un mensaje de error y se 
+//devuelve la estructura de la fecha
+//de tu día de nacimiento.
+fecha crear_fecha(short int anyo, short int mes, short int dia);
 
 #endif
